d72a66f3e9fab176f5489264c26d76996242c095
Laurenze Castro <laurenze.castro@valens-research.com>
2020-11-28 19:05:15 +0800

    Initial UI and API integration

---
+++ components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+    <h1 class="title">
+      pokemon-master
+    </h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,29 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        pokemon-master
-      </h1>
+    <div v-for="(pokemon, i) in pokemonData.results" :key="i">
+      <span>{{ pokemon.name }}</span>
+      <img :src="pokemon.url" style="height:150px" />
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  name: "index",
+
+  async asyncData({ $axios }) {
+    const pokemonData = await $axios.$get(
+      `http://localhost:3000/api/pagination/1`
+    );
+    return { pokemonData };
+  },
+
+  mounted() {
+    console.log(this.pokemonData);
+  }
+};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
