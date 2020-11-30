f4b0d956984cb21d6e177e89dcb1e76e950e13d1
Laurenze Castro <laurenze.castro@valens-research.com>
2020-11-30 17:32:14 +0800

    Added search functionality

--- components/Header.vue
+++ components/Header.vue
@@ -11,11 +11,16 @@
         type="text"
         class="search-box"
         placeholder="Search pokemon"
-        @input="$nuxt.$emit('searchKeyword', $event)"
+        @input="debounceSearch($event)"
       />
 
-      <div class="search-results">
-        <h5>Pikachu</h5>
+      <div class="search-results" :class="{ active: !isHidden }">
+        <h5 v-if="!isSuccess">{{ loadingMessage }}</h5>
+        <nuxt-link v-else :to="`/pokemon/${pokemonName.toLowerCase()}`">
+          <h5>
+            {{ pokemonName }}
+          </h5>
+        </nuxt-link>
       </div>
     </div>
   </header>
@@ -27,8 +32,64 @@ export default {
 
   data() {
     return {
+      pokemonName: "",
+      isHidden: true,
+      isSuccess: false,
+      loadingMessage: "Searching..",
+
+      // search
+      timeOut: "",
+      timeInterval: 500,
+      searchResult: [],
       searchKeyword: ""
     };
+  },
+
+  methods: {
+    async searchPokemon(keyword) {
+      this.searchResult = await this.$axios.$get(
+        `api/fetch_pkmn_details/${keyword}`
+      );
+
+      let { status } = this.searchResult;
+
+      if (status == "SUCCESS") {
+        this.isSuccess = true;
+        this.pokemonName = this.searchResult.pokemon.name;
+      } else if (status == "FAILED") {
+        this.loadingMessage = "No Pokemon Found.";
+      }
+    },
+
+    debounceSearch($event) {
+      this.isHidden = false;
+      this.isSuccess = false;
+      this.loadingMessage = "Searching...";
+
+      if (this.timeOut) {
+        clearTimeout(this.timeOut);
+      }
+
+      //After the timer, call the function
+      this.timeOut = setTimeout(() => {
+        this.searchPokemon($event.target.value);
+      }, this.timeInterval);
+      // }
+    }
+  },
+
+  created() {
+    this.isHidden = true;
+  },
+
+  watch: {
+    $route: {
+      handler() {
+        this.isHidden = true;
+        this.loadingMessage = "Searching...";
+      },
+      immediate: true
+    }
   }
 };
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -11,54 +11,8 @@ import Header from "@/components/Header";
 export default {
   name: "Layout",
 
-  data() {
-    return {
-      searchKeyword: "",
-
-      // search
-      timeOut: "",
-      timeInterval: 1000,
-      searchResult: []
-    };
-  },
-
-  methods: {
-    async searchPokemon() {
-      this.searchResult = await this.$axios.$get(
-        `http://localhost:3000/api/fetch_pkmn_details/${this.searchKeyword}`
-      );
-
-      let { status } = this.searchResult;
-
-      if (status == "SUCCESS") {
-        console.log(this.searchResult.pokemon.name);
-      } else if (status == "FAILED") {
-        console.log("FAILED");
-      }
-    },
-
-    debounceSearch(keyword) {
-      if (this.timeOut) {
-        clearTimeout(this.timeOut);
-      }
-
-      //After the timer, call the function
-      this.timeOut = setTimeout(() => {
-        this.searchPokemon();
-      }, this.timeInterval);
-      // }
-    }
-  },
-
   components: {
     Header
-  },
-
-  created() {
-    this.$nuxt.$on("searchKeyword", event => {
-      this.searchKeyword = event.target.value;
-      this.debounceSearch();
-    });
   }
 };
 </script>
